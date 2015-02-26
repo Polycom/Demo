022a8125a2b87209521a17aa164b892844e11f5b
Madhavi <Madhavi.Giddaluri@polycom.com>
2015-02-26 18:11:37 +0530

    Commiting

---
+++ My/src/main/java/mynew/MyNew.java
@@ -0,0 +1,10 @@
+package mynew;
+import org.apache.log4j.*;
+public class MyNew 
+{
+	static Logger logger=Logger.getLogger(MyNew.class);
+	public static void main(String args[])
+	{
+		System.out.println("Got it");
+	}
+}
